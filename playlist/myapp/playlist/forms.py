b6e5f0f5fffc5b6f4ec7b1a3249f75a8161680a6
shashwatthapa <thapashashwat@gmail.com>
2024-12-06 15:35:57 +0545

    code only for login page kept.Others are cleaned for now

--- playlist/myapp/playlist/forms.py
+++ playlist/myapp/playlist/forms.py
@@ -6,8 +6,6 @@ class LoginForm(forms.Form):
     username = forms.CharField(label="Username", max_length=150)
     password = forms.CharField(widget=forms.PasswordInput, label="Password")
 
-class SignupForm(UserCreationForm):
-    e
-    class Meta:
-        model = User
-        fields = ['username','password1','password2']
+
+
+    
